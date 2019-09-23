51b763a1121689857b353d8285b0b4910c4a8420
Yadir Hernandez Batista <yadirhb@gmail.com>
2019-09-23 15:17:30 -0500

    Added React app

--- twitter-clone-client/src/navigation/AppNavigator.js
+++
@@ -1,5 +0,0 @@
-import { createAppContainer } from 'react-navigation';
-
-import CommonNavigator from './CommonNavigator';
-
-export default createAppContainer(CommonNavigator);
--- twitter-clone-client/src/navigation/AppNavigator.web.js
+++
@@ -1,5 +0,0 @@
-import { createBrowserApp } from '@react-navigation/web';
-import CommonNavigator from './CommonNavigator';
-CommonNavigator.path = '';
-
-export default createBrowserApp(CommonNavigator, { history: 'hash' });
