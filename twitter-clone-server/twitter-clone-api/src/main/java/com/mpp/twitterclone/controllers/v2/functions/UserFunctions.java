580d2d31faa74d25c3484bc438c3c2c9e725def6
Yadir Hernandez Batista <yadirhb@gmail.com>
2019-09-25 11:12:54 -0500

    Added Users functions

--- twitter-clone-server/twitter-clone-api/src/main/java/com/mpp/twitterclone/controllers/v2/functions/UserFunctions.java
+++ twitter-clone-server/twitter-clone-api/src/main/java/com/mpp/twitterclone/controllers/v2/functions/UserFunctions.java
@@ -7,10 +7,12 @@ import com.mpp.twitterclone.model.Tweet;
 import com.mpp.twitterclone.model.User;
 import org.springframework.hateoas.Resource;
 
+import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BiFunction;
+import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 
 /**
@@ -19,55 +21,65 @@ import java.util.stream.Collectors;
 
 public class UserFunctions {
 
-	///> Pure Functions
-	// Convert User to Resource
-	public static BiFunction<User, UserResourceAssembler, Resource<User>> convertUserToResource =
-			(user, resourceAssembler) -> resourceAssembler.toResource(user);
+    ///> Pure Functions
+    // Convert User to Resource
+    public static BiFunction<User, UserResourceAssembler, Resource<User>> convertUserToResource =
+            (user, resourceAssembler) -> resourceAssembler.toResource(user);
 
-	// Convert User(s) to Resource(s)
-	public static BiFunction<List<User>, UserResourceAssembler, List<Resource<User>>> convertUsersToResources =
-			(users, resourceAssembler) -> users.stream()
-					.map(user -> convertUserToResource.apply(user, resourceAssembler))
-					.collect(Collectors.toList());
+    // Convert User(s) to Resource(s)
+    public static BiFunction<List<User>, UserResourceAssembler, List<Resource<User>>> convertUsersToResources =
+            (users, resourceAssembler) -> users.stream()
+                    .map(user -> convertUserToResource.apply(user, resourceAssembler))
+                    .collect(Collectors.toList());
 
-	// Find Top K Most Followed Users
-	public static BiFunction<List<User>, Long, List<User>> findTopKMostFollowedUsers =
-			(users, k) -> users.stream()
-					.sorted(Comparator.comparing(User::getFollowersCount).reversed())
-					.limit(k)
-					.collect(Collectors.toList());
+    // Find Top K Most Followed Users
+    public static BiFunction<List<User>, Long, List<User>> findTopKMostFollowedUsers =
+            (users, k) -> users.stream()
+                    .sorted(Comparator.comparing(User::getFollowersCount).reversed())
+                    .limit(k)
+                    .collect(Collectors.toList());
 
-	// Find K Most Posting Users
-	public static TriFunction<List<Tweet>, List<User>, Long, List<User>> findTopKMostPostingUsers =
-			(tweets, users, k) -> tweets.stream()
-					.filter(t -> t.getOwner() != null)
-					.collect(Collectors.groupingBy(Tweet::getOwner, Collectors.counting())).entrySet().stream()
-					.sorted((e1,e2) -> (int)(e2.getValue()-e1.getValue()))
-					.map(entry -> users.stream().filter(user -> user.getUsername().equals(entry.getKey())).findFirst())
-					.filter(Optional::isPresent)
-					.limit(k)
-					.map(Optional::get)
-					.collect(Collectors.toList());
+    // Find K Most Posting Users
+    public static TriFunction<List<Tweet>, List<User>, Long, List<User>> findTopKMostPostingUsers =
+            (tweets, users, k) -> tweets.stream()
+                    .filter(t -> t.getOwner() != null)
+                    .collect(Collectors.groupingBy(Tweet::getOwner, Collectors.counting())).entrySet().stream()
+                    .sorted((e1, e2) -> (int) (e2.getValue() - e1.getValue()))
+                    .map(entry -> users.stream().filter(user -> user.getUsername().equals(entry.getKey())).findFirst())
+                    .filter(Optional::isPresent)
+                    .limit(k)
+                    .map(Optional::get)
+                    .collect(Collectors.toList());
 
-	// Find K Most Followed Users with Replies to a Given Tweet
-	public static QuadFunction<List<Tweet>, List<User>, Tweet, Long, List<User>> findTopKMostFollowedRepliers =
-			(tweets, users, givenTweet, k) -> TweetFunctions.findRepliesByTweet.apply(tweets, givenTweet).stream()
-					.collect(Collectors.groupingBy(Tweet::getOwner, Collectors.counting()))
-					.entrySet().stream()
-					.sorted((e1, e2) -> (int)(e2.getValue() - e1.getValue()))
-					.map(entry -> users.stream().filter(u -> u.getUsername().equals(entry.getKey())).findFirst())
-					.filter(Optional::isPresent)
-					.map(Optional::get)
-					.sorted(Comparator.comparing(User::getFollowersCount, Comparator.reverseOrder()))
-					.limit(k)
-					.collect(Collectors.toList());
+    // Find K Most Followed Users with Replies to a Given Tweet
+    public static QuadFunction<List<Tweet>, List<User>, Tweet, Long, List<User>> findTopKMostFollowedRepliers =
+            (tweets, users, givenTweet, k) -> TweetFunctions.findRepliesByTweet.apply(tweets, givenTweet).stream()
+                    .collect(Collectors.groupingBy(Tweet::getOwner, Collectors.counting()))
+                    .entrySet().stream()
+                    .sorted((e1, e2) -> (int) (e2.getValue() - e1.getValue()))
+                    .map(entry -> users.stream().filter(u -> u.getUsername().equals(entry.getKey())).findFirst())
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .sorted(Comparator.comparing(User::getFollowersCount, Comparator.reverseOrder()))
+                    .limit(k)
+                    .collect(Collectors.toList());
 
-	// Find Tweet Repliers
-	public static TriFunction<List<Tweet>, List<User>, Tweet, List<User>> findRepliersOfGivenTweet =
-			(tweets, users, givenTweet) -> TweetFunctions.findRepliesByTweet.apply(tweets, givenTweet).stream()
-					.map(tweet -> users.stream().filter(u -> u.getUsername().equals(tweet.getOwner())).findFirst())
-					.filter(Optional::isPresent)
-					.map(Optional::get)
-					.distinct()
-					.collect(Collectors.toList());
+    // Find Tweet Repliers
+    public static TriFunction<List<Tweet>, List<User>, Tweet, List<User>> findRepliersOfGivenTweet =
+            (tweets, users, givenTweet) -> TweetFunctions.findRepliesByTweet.apply(tweets, givenTweet).stream()
+                    .map(tweet -> users.stream().filter(u -> u.getUsername().equals(tweet.getOwner())).findFirst())
+                    .filter(Optional::isPresent)
+                    .map(Optional::get)
+                    .distinct()
+                    .collect(Collectors.toList());
+
+    // User is young (15-24)
+	public static BiPredicate<User, LocalDate> userIsYoung = (user, today) -> Optional.ofNullable(user.getDateOfBirth()).isPresent() && ((today.getYear() - user.getDateOfBirth().getYear() > 15) && (today.getYear() - user.getDateOfBirth().getYear() < 24));
+
+	// Get Youth (15-24) users
+    public static BiFunction<List<User>, LocalDate, List<User>> getYouthCategoryUsers =
+            (users, today) -> users.stream()
+                    .filter(user -> userIsYoung.test(user, today))
+                    .collect(Collectors.toList());
+
 }
--- twitter-clone-server/twitter-clone-data/src/main/java/com/mpp/twitterclone/model/User.java
+++ twitter-clone-server/twitter-clone-data/src/main/java/com/mpp/twitterclone/model/User.java
@@ -1,6 +1,5 @@
 package com.mpp.twitterclone.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.mpp.twitterclone.enums.Gender;
 import com.mpp.twitterclone.enums.RoleName;
@@ -14,15 +13,14 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.hateoas.core.Relation;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Jonathan on 9/8/2019.
@@ -33,67 +31,67 @@ import java.util.*;
 @Document(collection = "users")
 @Relation(collectionRelation = "users") // To rename the default spring HATEOAS embedded list
 public class User {
-	@Id
-	private String id;
+    @Id
+    private String id;
 
-	@Indexed(unique = true)
-	@Size(min = 1, max = 20, message = "Username Must be Between 1 and 20 Characters")
-	@NotEmpty(message = "Username is Required")
-	private String username;
+    @Indexed(unique = true)
+    @Size(min = 1, max = 20, message = "Username Must be Between 1 and 20 Characters")
+    @NotEmpty(message = "Username is Required")
+    private String username;
 
-	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-	@Size(min = 6, message = "Password Must be a Minimum of 6 Characters")
-	@NotEmpty(message = "Password is Required")
-	private String password;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @Size(min = 6, message = "Password Must be a Minimum of 6 Characters")
+    @NotEmpty(message = "Password is Required")
+    private String password;
 
-	@Size(max = 50, message = "Name Must be a Maximum of 50 Characters")
-	private String name;
+    @Size(max = 50, message = "Name Must be a Maximum of 50 Characters")
+    private String name;
 
-	@Indexed(unique = true)
-	@NotEmpty(message = "Email is Required")
-	private String email;
+    @Indexed(unique = true)
+    @NotEmpty(message = "Email is Required")
+    private String email;
 
-	private Gender gender;
+    private Gender gender;
 
-	@Field(value = "date_of_birth")
-	private LocalDate dateOfBirth;
+    @Field(value = "date_of_birth")
+    private LocalDate dateOfBirth;
 
-	@URL(message = "URL Must be in a Standard Format")
-	@Field(value = "profile_banner_url")
-	private String profileBannerUrl;
+    @URL(message = "URL Must be in a Standard Format")
+    @Field(value = "profile_banner_url")
+    private String profileBannerUrl;
 
-	@URL(message = "URL Must be in a Standard Format")
-	@Field(value = "profile_image_url")
-	private String profileImageUrl;
+    @URL(message = "URL Must be in a Standard Format")
+    @Field(value = "profile_image_url")
+    private String profileImageUrl;
 
-	@Field(value = "phone_number")
-	private String phoneNumber;
+    @Field(value = "phone_number")
+    private String phoneNumber;
 
-	@URL(message = "URL Must be in a Standard Format")
-	private String url;
+    @URL(message = "URL Must be in a Standard Format")
+    private String url;
 
-	private String description;
+    private String description;
 
-	@Field(value = "protected")
-	private Boolean protect;
+    @Field(value = "protected")
+    private Boolean protect;
 
-	private Boolean verified;
+    private Boolean verified;
 
-	@Field(value = "followers_count")
-	@Builder.Default
-	private Integer followersCount = 0;
+    @Field(value = "followers_count")
+    @Builder.Default
+    private Integer followersCount = 0;
 
-	@Field(value = "friends_count")
-	@Builder.Default
-	private Integer friendsCount = 0;
+    @Field(value = "friends_count")
+    @Builder.Default
+    private Integer friendsCount = 0;
 
-	@DBRef
-	@Builder.Default
-	private Set<Role> roles = new HashSet<>(Arrays.asList(Role.builder().name(RoleName.USER).build()));
+    @DBRef
+    @Builder.Default
+    private Set<Role> roles = new HashSet<>(Arrays.asList(Role.builder().name(RoleName.USER).build()));
 
-	@Field(value = "created_at")
-	@CreatedDate
-	@Builder.Default
-	private LocalDateTime createdAt = LocalDateTime.now();
+    @Field(value = "created_at")
+    @CreatedDate
+    @Builder.Default
+    private LocalDateTime createdAt = LocalDateTime.now();
 
 }
